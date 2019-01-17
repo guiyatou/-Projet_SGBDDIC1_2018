c0e3374ebd7dc7d5d6ec97f9f3ec3db49fa92074
khadija23 <46113859+khadija23@users.noreply.github.com>
2019-01-17 03:28:13 +0000

    Add files via upload

---
+++ xml_validation.py
@@ -0,0 +1,24 @@
+import xmL_extraction
+import xml.etree.ElementTree as ET
+#Projet_SGBD/XJ_Convertor.py
+
+
+# analyse du fichier xml
+def parsefile(file):
+  return ET.parse(file)
+
+#validation du fichier xml
+def xml_validator(file):
+  try:
+    parsedFile = parsefile(file)
+    print(parsedFile)
+    return parsedFile.getroot()
+  except Exception as e:
+    print("---Erreur: le fichier xml %s n'a pas une bonne syntaxe" % file)
+    print(" voila c'est ca l'erreur %s" %e)
+    return False
+
+if __name__ == "__main__":
+  myXmlFile = xml_validator("fichier.xml")
+  extractedXmlFile = xml_extraction.extractXmlFile(myXmlFile)
+  print(extractedXmlFile)
